9acbf4043ad024605da917746f35cbb88775334e
Alan Crisanto <alanvcrisanto@gmail.com>
2021-12-22 17:22:02 -0500

    first commit

---
+++ products.py
@@ -0,0 +1,17 @@
+products = [
+    {
+        "name": "laptop",
+        "price": 1200,
+        "quantity": 4,
+    },
+    {
+        "name": "mouse",
+        "price": 50,
+        "quantity": 10,
+    },
+    {
+        "name": "monitor",
+        "price": 300,
+        "quantity": 5,
+    }
+]
